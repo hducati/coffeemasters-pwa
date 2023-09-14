812688b8fdde0dea0810d36ea4e4d74092a32259
Henrique Ducati <henrique.dcti@gmail.com>
2023-09-13 21:09:58 -0300

    feat: add quota and persistance examples

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,23 @@ import Menu from './Menu.js';
 import Order from './Order.js';
 import Router from './Router.js';
 
+// Request persistence storage
+(async function() {
+    if (navigator.storage && navigator.storage.persist) {
+        if (!await navigator.storage.persisted()) {
+            const result = await navigator.storage.persist();
+            console.log(`Persist result: ${result}`)
+        }
+    }
+
+    // Quota
+    if (navigator.storage && navigator.storage.estimate){
+        const q = await navigator.storage.estimate();
+        console.log(`Quota available: ${q.quota/1024/1024}MiB`)
+        console.log(`Quota used: ${q.usage/1024}KiB`)
+    }
+})();
+
 window.addEventListener("DOMContentLoaded", () => {
     Router.init();
     Menu.load();
